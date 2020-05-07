921e2980d04713f40c458889b596f0bb9649fb35
Carla Holcomb <carla.programmer@gmail.com>
2020-05-07 00:06:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
   
   def destroy
   session.delete :username
+  session.delete :username
   end
   
    def create
         session[:username] = params[:username]
+        redirect_to '/'
+        
+         params[:username] == nil
         redirect_to '/'
     end
 end
