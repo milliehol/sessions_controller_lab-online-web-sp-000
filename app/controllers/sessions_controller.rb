03718498f91c7481e64f36c976a4f434ff348c2b
Carla Holcomb <carla.programmer@gmail.com>
2020-05-07 00:03:16 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def destroy
+  session.delete :username
+  end
+  
+   def create
+        session[:username] = params[:username]
+        redirect_to '/'
+    end
 end
